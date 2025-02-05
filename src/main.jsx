92a4c3b7988007c88290927521eb94bc3300fb1d
alinasr783 <alinasreldin783@gmail.com>
2025-02-05 22:18:00 +0000

    meem store

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <BrowserRouter basename="/zadsudan">
+      <BrowserRouter basename="/meem-store">
         <App />
       </BrowserRouter>
   </StrictMode>
